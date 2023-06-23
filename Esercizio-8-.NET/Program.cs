96f954de8b13c68bfe8b3413720c84794a49e3e7
emifa <emifa@LAPTOP-1TM449QB>
2023-06-23 16:32:06 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-8-.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using Esercizio_8_.NET;
+
+Console.WriteLine("Please enter the word");
+string input = Console.ReadLine();
+ProgrammPalindromo programmPalindromo = new ProgrammPalindromo();
+
+if (programmPalindromo.IsPalindromo(input)) 
+    {
+        Console.WriteLine("It's a palindrome");
+    }
+    else
+    {
+        Console.WriteLine("It is not a palindrome");
+    }
+
---
+++ Esercizio-8-.NET/ProgrammPalindromo.cs
@@ -0,0 +1,26 @@
+﻿namespace Esercizio_8_.NET
+{
+    internal class ProgrammPalindromo
+    {
+        public bool IsPalindromo(string input)
+        {
+            string cleanedInput = input.Replace(" ","").ToLower();
+
+            int first = 0;  
+            int last = cleanedInput.Length - 1;
+
+            while (first < last) 
+            {
+                if (cleanedInput[first] != cleanedInput[last])
+                {
+                    return false;
+                }    
+
+                first++;
+                last--;
+            }
+
+            return true;
+        }
+    }
+}
